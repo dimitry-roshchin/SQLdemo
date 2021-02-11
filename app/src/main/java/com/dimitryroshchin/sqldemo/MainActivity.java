88385368679c418378c22fbcb6d4e0a5fb3ef4ba
dimitry roshchin <dimitry.roshchin@outlook.com>
2021-02-11 13:03:03 -0600

    Add button click listeners

--- app/src/main/java/com/dimitryroshchin/sqldemo/MainActivity.java
+++ app/src/main/java/com/dimitryroshchin/sqldemo/MainActivity.java
@@ -3,12 +3,15 @@ package com.dimitryroshchin.sqldemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //references to buttons and other controls on the layout
-    androidx.appcompat.widget.AppCompatButton btn_add, btn_viewAll;
+    Button btn_add, btn_viewAll;
     EditText et_name, et_age;
     androidx.appcompat.widget.SwitchCompat sw_active;
     androidx.recyclerview.widget.RecyclerView rv_customerList;
@@ -19,5 +22,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        et_name = findViewById(R.id.et_name);
+        et_age = findViewById(R.id.et_age);
+        sw_active = findViewById(R.id.sw_active);
+        rv_customerList = findViewById(R.id.rv_customerList);
+
+        // button click listeners for the add and view all buttons
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Add Button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "View All Button", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
