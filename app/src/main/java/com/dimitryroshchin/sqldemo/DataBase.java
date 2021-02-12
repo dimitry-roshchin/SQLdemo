b52d6b9a91d0a0956bc9cb064389142af4a17169
dimitry roshchin <dimitry.roshchin@outlook.com>
2021-02-11 22:47:57 -0600

    initial database setup

---
+++ app/src/main/java/com/dimitryroshchin/sqldemo/DataBase.java
@@ -0,0 +1,50 @@
+package com.dimitryroshchin.sqldemo;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DataBase extends SQLiteOpenHelper {
+
+
+    public static final String CUSTOMER_TABLE = "CUSTOMER_TABLE";
+    public static final String COLUMN_CUSTOMER_NAME = "CUSTOMER_NAME";
+    public static final String COLUMN_CUSTOMER_AGE = "CUSTOMER_AGE";
+    public static final String COLUMN_ACTIVE_CUSTOMER = "ACTIVE_CUSTOMER";
+    public static final String COLUMN_ID = "ID";
+
+
+    public DataBase(@Nullable Context context) {
+        super(context, "customer.db", null , 1);
+    }
+
+    //this is called the first time a database is accessed. There should be code in here to create a new database.
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + CUSTOMER_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_CUSTOMER_NAME + " TEXT, " + COLUMN_CUSTOMER_AGE + " INT, " + COLUMN_ACTIVE_CUSTOMER + " BOOL)";
+
+        db.execSQL(createTableStatement);
+    }
+
+    // this is called if the database version number changes. It prevents previous users apps from breaking when you change the database design.
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    public boolean addOne(CustomerModel customerModel) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_CUSTOMER_NAME, customerModel.getName());
+        cv.put(COLUMN_CUSTOMER_AGE, customerModel.getAge());
+        cv.put(COLUMN_ACTIVE_CUSTOMER, customerModel.isActive());
+
+        long insert = db.insert(CUSTOMER_TABLE, null, cv);
+        return insert != -1;
+
+
+    }
+}
--- app/src/main/java/com/dimitryroshchin/sqldemo/MainActivity.java
+++ app/src/main/java/com/dimitryroshchin/sqldemo/MainActivity.java
@@ -34,14 +34,24 @@ public class MainActivity extends AppCompatActivity {
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                CustomerModel customerModel;
+
                 try {
-                    CustomerModel customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_active.isChecked());
+                    customerModel  = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_active.isChecked());
                     Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
 
                 }
                 catch (Exception e) {
                     Toast.makeText(MainActivity.this, "Error Creating Customer", Toast.LENGTH_SHORT).show();
+                    customerModel = new CustomerModel(-1, "error", 0, false);
                 }
+
+                DataBase dataBase = new DataBase((MainActivity.this));
+
+                boolean success = dataBase.addOne(customerModel);
+
+                Toast.makeText(MainActivity.this, "success=" + success, Toast.LENGTH_SHORT).show();
             }
         });
 
